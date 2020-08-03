430f779e1b475a1b88386086f42b69b07a7f4bd4
MacGiv <tomasluchelli.17@gmail.com>
2020-08-03 15:47:55 -0300

    Old Controls Script Removed

--- Assets/Scripts/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/PlayerFiniteStateMachine/Player.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     #region State Variables
+
     public PlayerStateMachine StateMachine { get; private set; }
     public PlayerIdleState IdleState { get; private set; }
     public PlayerMoveState MoveState { get; private set; }
     #endregion
 
     #region Components
+
     public Animator Anim { get; private set; }
     public PlayerInputHandler InputHandler { get; private set; }
     public Rigidbody2D RB { get; private set; }
     #endregion
 
     #region Other Variables
+
     public Vector2 CurrentVelocity { get; private set; }
     public int FacingDirection { get; private set; }
 
